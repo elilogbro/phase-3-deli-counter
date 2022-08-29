8ac80970613546c0d54b680d27221b28c03a5073
Eli <+>
2022-08-29 12:44:56 -0400

    commit tree 1a95855171505e0483fb566ddcaa83d2475bf40c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line(array, name)
+    if array.length == 0
+        puts "The line is currently empty."
+    elsif array.length > 0
+        puts "The line is currently: #{array.length}. #{name} #{array.find_index(name)}"
+    end
+end
+
+def take_a_number(array, name)
+    number = 0
+    puts "Welcome, #{name}. You are number #{number += 1} in line."
+    array << name
+end
